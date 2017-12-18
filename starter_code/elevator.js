63cbc18b4ecbbbf15c089737375133e35cd18fd7
Juancho Arregui <juan.arregui.lopez@gmail.com>
2017-12-18 16:47:58 +0100

    feat: iteration 3 finished

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,84 @@
+const Person = require('./person.js');
+
 class Elevator {
+
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
-    this.requests   = [];
+    this.request   = [];
+    this.direction = "up";
+    this.movement;
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.movement = setInterval(() => this.update(), 1000);
+  }
+
+  stop() {
+    clearInterval(this.movement)
+  }
+
+  update() {
+    this.log(this.floor)
+  }
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.direction = "up";
+      this.floor++
+    }
+  }
+
+  floorDown() {
+    if (this.floor > 0) {
+      this.direction = "down";
+      this.floor--
+    }
+  }
+
+  move(destinationFloor){
+    let path = destinationFloor - this.floor;
+    if (path > 0) {
+      for (let p = this.floor; p < this.MAXFLOOR; p++ ) {
+        this.floorUp();
+        this.log();
+      }
+      this.logDestination();
+    }
+    else if (path < 0) {
+      for (let p = this.floor; p > 0; p--) {
+        this.floorDown();
+        this.log();
+      }
+      this.logDestination();
+    }
+    else {
+      console.log(`the elevator is already on floor ${this.floor}`)
+    }  
+  }
+
+
+  _passengersEnter() { 
+    
+      }
+    
+  _passengersLeave() { 
+
+  }
+
+  call(person) {
+    this.request.push(person);
+  }
+
+  log() {
+    console.log(`Moving Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
+
+  logDestination() {
+    console.log(`The elevator has reached the destination floor. Floor: ${this.floor}`);
+  }
+  
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
 const Elevator = require('./elevator.js');
+
+
+let elevator = new Elevator();
+
+elevator.move(10);
+elevator.move(0);
+
